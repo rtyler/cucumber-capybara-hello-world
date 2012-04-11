b2dc6fb7a20ae637c5ba11d3dcf17627dfc41e2b
R. Tyler Croy <tyler@monkeypox.org>
2012-04-11 11:52:48 -0700

    Add the boiler-plate pending steps

---
+++ features/step_definitions/demo_steps.rb
@@ -0,0 +1,7 @@
+When /^I visit the home page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be elated with results$/ do
+  pending # express the regexp above with the code you wish you had
+end
