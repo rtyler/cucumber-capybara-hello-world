f5334813b2bafb443a4aa547fb395dc31970b19d
R. Tyler Croy <tyler@monkeypox.org>
2012-04-11 11:58:44 -0700

    Implement the first step definition, which just visits jenkins-ci.org in the browser

--- features/step_definitions/demo_steps.rb
+++ features/step_definitions/demo_steps.rb
@@ -1,5 +1,7 @@
 When /^I visit the home page$/ do
-  pending # express the regexp above with the code you wish you had
+  visit 'https://jenkins-ci.org'
+  # Sleeping here just for demonstration purposes, so we can see the browser come up
+  #sleep 15
 end
 
 Then /^I should be elated with results$/ do
